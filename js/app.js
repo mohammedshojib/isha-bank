6c982922b1778fe02527f3990fbe4e1e0da98328
mohammedshojib <asmdshojib11@gmail.com>
2022-02-17 14:59:17 +0600

    netlify uodated

--- js/app.js
+++ js/app.js
@@ -1,45 +1,59 @@
 document
   .getElementById("calculateIncome")
   .addEventListener("click", function () {
-    const income = parseInt(document.getElementById("income").value);
+    const income = document.getElementById("income");
     const balance = document.getElementById("balance");
     const expenses = document.getElementById("expenses");
-    const food = parseInt(document.getElementById("food").value);
-    const rent = parseInt(document.getElementById("rent").value);
-    const clothes = parseInt(document.getElementById("clothes").value);
+    const food = document.getElementById("food");
+    const rent = document.getElementById("rent");
+    const clothes = document.getElementById("clothes");
 
-    expenses.innerText = food + rent + clothes;
-    balance.innerText = income - expenses.innerText;
-    if (expenses.innerText > income) {
-      alert("Please input your correct income");
+    const incomeValue = parseFloat(income.value);
+    const foodValue = parseFloat(food.value);
+    const rentValue = parseFloat(rent.value);
+    const clothesValue = parseFloat(clothes.value);
+
+    expenses.innerText = foodValue + rentValue + clothesValue;
+    balance.innerText = incomeValue - expenses.innerText;
+    if (expenses.innerText > incomeValue) {
+      alert("Please input your correct income / Expenses");
       balance.innerText = "Plz input correct icome";
     } else if (
-      income == "" ||
-      income < 0 ||
-      food == "" ||
-      food < 0 ||
-      rent == "" ||
-      rent < 0 ||
-      clothes == "" ||
-      clothes < 0
+      income.value == "" ||
+      income.value == String ||
+      income.value <= 0 ||
+      food.value == "" ||
+      food.value == String ||
+      food.value < 0 ||
+      rent.value == "" ||
+      rent.value == String ||
+      rent.value < 0 ||
+      clothes.value == String ||
+      clothes.value == "" ||
+      clothes.value < 0
     ) {
       alert("Please input your correct informationd.");
     }
   });
+/* ----------savings area------ */
 
 document.getElementById("saveBtn").addEventListener("click", function () {
-  const saveAmount = parseInt(document.getElementById("savingAmount").value);
+  const save = document.getElementById("savingAmount");
   const savings = document.getElementById("savings");
-  const income = parseInt(document.getElementById("income").value);
+  const income = document.getElementById("income");
   const balance = document.getElementById("balance");
   const remainingBalance = document.getElementById("remainingBalance");
 
-  savings.innerText = saveAmount * (income / 100);
+  const saveAmount = parseFloat(save.value);
+  const incomeValue = parseInt(income.value);
+  savings.innerText = saveAmount * (incomeValue / 100);
   remainingBalance.innerText = balance.innerText - savings.innerText;
 
-  // if (remainingBalance.innerText < 0) {
-  //   alert("Please input your correct saving informationd.");
-  //   remainingBalance.innerText =
-  //     "Please input your correct saving informationd.";
-  // }
+  if (remainingBalance.innerText < 0) {
+    alert("Please input your correct saving informationd.");
+    remainingBalance.innerText =
+      "Please input your correct saving informationd.";
+  } else if (save.value == "" || save.value == String || save.value < 0) {
+    alert("Please input your correct saving informationd.");
+  }
 });
